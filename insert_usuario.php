eb5a4039650784993ef79747b1d0e823d7fbd394
NickzinhoReidelas411 <167816335+NickzinhoReidelas411@users.noreply.github.com>
2025-10-01 15:01:49 -0300

    Boa noite mundo!

---
+++ insert_usuario.php
@@ -0,0 +1,37 @@
+<?php
+// incluir o arquivo de conexão
+include 'conexao.php';
+
+$nm_usuario    = $_POST['nome'];
+$dt_nascimento = $_POST['data'];
+$login         = $_POST['login'];
+$senha_usuario = $_POST['senha'];
+$setor         = $_POST['setor'];
+
+// função para inserir os dados do usuário
+$insert = "INSERT INTO tb_usuario values (
+    null,
+    '$nm_usuario',
+    '$dt_nascimento',
+    '$login',
+    '$senha_usuario',
+    '$setor'
+)";
+
+$query = $conexao->query($insert);
+
+if ($query)
+{
+    echo "<script>
+        alert('Usuário cadastrado com sucesso!');
+        location.href = '../index.html';
+    </script>";
+}
+else
+{
+    echo "<script>
+        alert('Erro ao cadastrar');
+        location.href = '../index.html';
+    </script>";
+}
+?>
